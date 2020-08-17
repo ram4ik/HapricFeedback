1a6e02f0fddf920d995ea9f0fb875be10dd6f910
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-17 19:55:14 +0300

    Initial Commit

---
+++ HapricFeedback/HapricFeedbackApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapricFeedbackApp.swift
+//  HapricFeedback
+//
+//  Created by Ramill Ibragimov on 17.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct HapricFeedbackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
